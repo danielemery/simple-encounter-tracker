5e3b04f9f733d3f318e77013d7d89339a704f19e
Daniel Emery <danielremery@gmail.com>
2018-09-12 13:56:40 +0930

    added basic graphql api with amplify cli

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,7 +8,10 @@ const awsmobile =  {
     "aws_cognito_identity_pool_id": "ap-southeast-2:9dd15ccb-d5d0-4634-a177-6195416a761e",
     "aws_cognito_region": "ap-southeast-2",
     "aws_user_pools_id": "ap-southeast-2_CaIJfM3rd",
-    "aws_user_pools_web_client_id": "74m74te6dr0ppmdagshjkjqhmg"
+    "aws_user_pools_web_client_id": "74m74te6dr0ppmdagshjkjqhmg",
+    "aws_appsync_graphqlEndpoint": "https://ndrom2n4wbhpzkblcklc2swzje.appsync-api.ap-southeast-2.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-southeast-2",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 
