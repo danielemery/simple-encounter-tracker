536091105abe56e3c5beca7681adad081804e556
Daniel Emery <danielremery@gmail.com>
2018-09-12 13:11:30 +0930

    Added auth with amplify cli

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,7 +4,11 @@ const awsmobile =  {
     "aws_project_region": "ap-southeast-2",
     "aws_content_delivery_bucket": "encountertracker-20180912125712--hostingbucket",
     "aws_content_delivery_bucket_region": "ap-southeast-2",
-    "aws_content_delivery_url": "http://encountertracker-20180912125712--hostingbucket.s3-website-ap-southeast-2.amazonaws.com"
+    "aws_content_delivery_url": "http://encountertracker-20180912125712--hostingbucket.s3-website-ap-southeast-2.amazonaws.com",
+    "aws_cognito_identity_pool_id": "ap-southeast-2:9dd15ccb-d5d0-4634-a177-6195416a761e",
+    "aws_cognito_region": "ap-southeast-2",
+    "aws_user_pools_id": "ap-southeast-2_CaIJfM3rd",
+    "aws_user_pools_web_client_id": "74m74te6dr0ppmdagshjkjqhmg"
 };
 
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Amplify, { Auth } from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
